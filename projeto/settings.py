fa969562d5974b6d42493fad07c89f549185e772
wagner <wagnercl31@hotmail.com>
2020-05-08 17:32:17 -0300

    cadastrar produto

--- projeto/settings.py
+++ projeto/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #apps terceiros
+    'widget_tweaks',
     #my apps
     'projeto.core',
     'projeto.produto',
