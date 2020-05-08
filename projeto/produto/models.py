7f8ee6fcceb3d2e9efc08f880f6eca507be9deb8
wagner <wagnercl31@hotmail.com>
2020-05-08 15:03:35 -0300

    produto_detail criado

--- projeto/produto/models.py
+++ projeto/produto/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse_lazy
 
 class Produto(models.Model):
     produto = models.CharField(max_length=100, unique=True)
@@ -11,5 +12,10 @@ class Produto(models.Model):
     
     def __str__(self):
         return self.produto
+
+
+    def get_absolute_url(self):
+        return reverse_lazy('produto:produto_detail', kwargs={'pk': self.pk})
+    
         
     
