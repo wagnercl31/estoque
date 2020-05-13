ab6c8856deb31d42832b19035ae06cb7587457dc
wagner <wagnercl31@hotmail.com>
2020-05-12 22:01:57 -0300

    estoque saida html

--- projeto/estoque/views.py
+++ projeto/estoque/views.py
@@ -1,28 +1,38 @@
 from django.shortcuts import render, resolve_url
 from django.forms import inlineformset_factory
 from django.http import HttpResponseRedirect
-from .models import Estoque, EstoqueItens
+from projeto.produto.models import Produto
+from .models import EstoqueEntrada, EstoqueSaida, EstoqueItens
 from .forms import EstoqueForm, EstoqueItensForm
 
 def estoque_entrada_list(request):
   template_name = 'estoque_entrada_list.html'
-  objects = Estoque.objects.filter(movimento='e')
+  objects = EstoqueEntrada.objects.all()
   context = {'object_list': objects}
   return render(request, template_name, context)
 
 
 def estoque_entrada_detail(request, pk):
   template_name = 'estoque_entrada_detail.html'
-  obj = Estoque.objects.get(pk=pk)
+  obj = EstoqueEntrada.objects.get(pk=pk)
   context = {'object': obj}
   return render(request, template_name, context)
 
 
+def dar_baixa_estoque(form):
+  produtos = form.estoques.all()
+  for item in produtos:
+    produto = Produto.objects.get(pk=item.produto.pk)
+    produto.estoque = item.saldo
+    produto.save()
+  print('Estoque atualizado com sucesso.')
+
+
 def estoque_entrada_add(request):
   template_name = 'estoque_entrada_form.html'
-  estoque_form = Estoque()
+  estoque_form = EstoqueEntrada()
   item_estoque_formset = inlineformset_factory(
-    Estoque,
+    EstoqueEntrada,
     EstoqueItens,
     form=EstoqueItensForm,
     extra=0,
@@ -39,11 +49,19 @@ def estoque_entrada_add(request):
     if form.is_valid() and formset.is_valid():
       form = form.save()
       formset.save()
+      dar_baixa_estoque(form)
       url = 'estoque:estoque_entrada_detail'
       return HttpResponseRedirect(resolve_url(url, form.pk))
   else:
-    form = EstoqueForm(instance=estoque_form, prefix='main')
-    formset =  item_estoque_formset(instance=estoque_form, prefix='estoque')
+      form = EstoqueForm(instance=estoque_form, prefix='main')
+      formset =  item_estoque_formset(instance=estoque_form, prefix='estoque')
   
   context = {'form': form, 'formset': formset}
+  return render(request, template_name, context)
+
+
+def estoque_saida_list(request):
+  template_name = 'estoque_saida_list.html'
+  objects = EstoqueSaida.objects.all()
+  context = {'object_list': objects}
   return render(request, template_name, context)
